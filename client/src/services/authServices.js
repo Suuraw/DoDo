d0822681e30ab2b489019742da7013c7fde6de91
Sujay kumar <sujayraw13@gmail.com>
2024-10-20 11:01:13 +0530

    finding the issue in request services

--- client/src/services/authServices.js
+++ client/src/services/authServices.js
@@ -1,12 +1,16 @@
 import axios from "axios";
 // import env from "dotenv";
 // env.config();//
-const SERVER_URL ="https://doodo.vercel.app/api";
+const SERVER_URL =
+  process.env.REACT_APP_SERVER_URL || "https://doodo.vercel.app/api";
+
 const registerUser = (data) => {
+  console.log("Register URL:", SERVER_URL + "/register");
   return axios.post(SERVER_URL + "/register", data);
 };
 
 const loginUser = (data) => {
+  console.log("Login URL:", SERVER_URL + "/login");
   return axios.post(SERVER_URL + "/login", data);
 };
 
